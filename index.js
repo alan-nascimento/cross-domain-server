22b2bb57c3799677c30bcee39cc0b74f5ee51c26
Alan Nascimento <alan.silva.n@hotmail.com>
2020-08-14 13:30:03 -0300

    fix: try send html file

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ app.use(cors())
 
 app.use(express.static(path.resolve(__dirname, './public')))
 
+app.use('/', (req, res) => res.sendFile('./public/index.html', { root: __dirname }))
+
 app.listen(3000, () => console.log('Server running'))
