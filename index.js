a17fed3cb1e77b29d9eaf5e21ed49228a3ff2568
Alan Nascimento <alan.n@dasa.com.br>
2020-08-14 13:15:56 -0300

    chore: setup project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.static(path.resolve(__dirname, './public')))
+
+app.listen(3000, () => console.log('Server running'))
